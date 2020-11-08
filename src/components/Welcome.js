f1aed4214e630bd260dbda940f4b2f95790b715e
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-11-08 23:35:10 +0530

    change3

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ import "../styles/App.css";
 const Welcome = ({ name }) => {
   return (
     <>
-      <h1>Hey ! {name} </h1>
+      <h1>Hey {name}! </h1>
       <h2>Welcome to Newton School.</h2>
     </>
   );
