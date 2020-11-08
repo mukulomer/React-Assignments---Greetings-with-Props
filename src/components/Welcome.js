4e77b81d6d2192142b5c0fcfd7800f2ad4119b62
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-11-08 23:07:26 +0530

    chnage1

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+import React from "react";
+import "../styles/App.css";
+
+const Welcome = ({ name }) => {
+  return (
+    <>
+      <h1>Hey !{name} </h1>
+      <h2>Welcome to Newton School.</h2>
+    </>
+  );
+};
+
+export default Welcome;
